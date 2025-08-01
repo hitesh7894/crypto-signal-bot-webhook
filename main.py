25421815118f1fd10b9bf8ea918dba91b242b3d2
hitesh7894 <108324045+hitesh7894@users.noreply.github.com>
2025-08-01 19:18:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,4 +46,5 @@ def root():
     return "✅ Crypto Signal Bot is Live"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 10000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
